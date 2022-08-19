6f144fa8ec0145aec0b729733ca5e2c1532c5580
larguesa <rlarguesa@gmail.com>
2022-08-19 19:46:31 -0300

    Testando o template string

--- index.ts
+++ index.ts
@@ -1,3 +1,14 @@
-// Write TypeScript code!
+let title: string = 'MyApp';
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+appDiv.innerHTML = `
+<h1>${title}</h1>
+<hr/>
+<p>Parágrafo qualquer...</p>
+`;
+
+console.log('Olá, mundo!');
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
